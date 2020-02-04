7c0fcdc9597e1c32061a7f28a078fe545ee3810a
Anna Agte <matiasrustagte@gmail.com>
2020-02-04 21:52:50 +0500

    refactor: reduce files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,43 @@
 <template>
   <v-container class="mx-auto" style="width: 600px">
     <h1 class="display-1 my-5 text-center">Настольные игры онлайн</h1>
-    <game-kinds></game-kinds>
+    <v-card
+      v-for="gameKind in items"
+      :key="gameKind.id"
+      class="mx-auto"
+      width="480"
+    >
+      <v-card-title>{{ gameKind.name }}</v-card-title>
+      <v-card-actions>
+        <v-btn
+          :to="{ name: 'WaitingGames', params: { gameKind: gameKind.id } }"
+          class="ml-auto"
+          color="primary"
+        >
+          Перейти
+          <v-icon>mdi-arrow-right</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
 
 <script>
-import GameKinds from '../components/GameKinds.vue';
+import { useFind } from 'feathers-vuex';
 
 export default {
-  components: { GameKinds },
+  setup(props, context) {
+    const { items } = useFind({
+      model: context.root.$FeathersVuex.api.GameKind,
+      params: {
+        query: {
+          $sort: { createdAt: -1 },
+          $skip: 0,
+          $limit: 50,
+        },
+      },
+    });
+    return { items };
+  },
 };
 </script>
